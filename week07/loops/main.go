ff963357d89565f168c613b5a7fc5e5692c4fd00
KaL <KaL1222kl@gmail.com>
2025-10-13 18:31:10 +0900

    loops/main.go v0.1 math/rand

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Enter a score")
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n')
-	if err != nil {
-		log.Fatal((err))
-	}
-
-	i = strings.TrimSpace(i)
-	score, err := strconv.ParseFloat(i, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var status string
-	if score >= 60 {
-		status = "Pass"
-	} else {
-		status = "Fail"
-	}
-	fmt.Println(score, status)
-	/*
-		var float64 float64 = 2.71
-		var f float64 = 3.991
-		fmt.Println(float64)
-		fmt.Println(f)
-		var fmt float64 = 2.71
-		fmt.Println(fmt)
-	*/
+	dice := rand.Intn(6) + 1 // 1~6
+	fmt.Println(dice)
 }
