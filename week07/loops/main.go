6720aa7173ce966a1128db4f5fad61b0fad3d5dd
KaL <KaL1222kl@gmail.com>
2025-10-13 17:50:15 +0900

    loops/main.go v0.1 bufio, os, err  packages

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n')
+	//i, _ := r.ReadString('\n')
+	i, err := r.ReadString('\n')
+	//fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(i)
 }
