4a3c15df0b1ed7163dbc8b242bc5b930b1de3d3c
KaL <KaL1222kl@gmail.com>
2025-10-13 18:06:25 +0900

    loops/main.go v0.1 if_2

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -5,15 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Enter a score")
 	r := bufio.NewReader(os.Stdin)
-	//i, _ := r.ReadString('\n')
 	i, err := r.ReadString('\n')
-	//fmt.Println(err)
+	if err != nil {
+		log.Fatal((err))
+	}
+
+	i = strings.TrimSpace(i)
+	score, err := strconv.ParseFloat(i, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(i)
+	var status string
+	if score >= 60 {
+		status = "Pass"
+	} else {
+		status = "Fail"
+	}
+	fmt.Println(score, status)
+	/*
+		var float64 float64 = 2.71
+		var f float64 = 3.991
+		fmt.Println(float64)
+		fmt.Println(f)
+		var fmt float64 = 2.71
+		fmt.Println(fmt)
+	*/
 }
