c521fe09e768b8557ffed27247e0c36dd743dacb
KaL <KaL1222kl@gmail.com>
2025-11-03 17:42:00 +0900

    week10 package v0.4

--- week10/pkg/greeting/korean/korean.go
+++ week10/pkg/greeting/korean/korean.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func Anyunghaseyo() {
 	fmt.Println("안녕하세요!")
+	// anyung() 같은 함수 내에선 접근 가능
 }
 
+// func anyung() { // 함수명이 소문자로 시작하는 경우 외부 파일에서 접근이 불가
 func Anyung() {
 	fmt.Println("안녕!")
 }
