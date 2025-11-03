d96f6471f7c3826ca741fd80f338f1a5ac3efc0d
KaL <KaL1222kl@gmail.com>
2025-11-03 17:17:00 +0900

    week10 package v0.2

--- week10/cmd/ch04-ex01/main.go
+++ week10/cmd/ch04-ex01/main.go
@@ -1,18 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"week10/pkg/keyboard"
-) // custom package
+import "week10/pkg/greeting"
 
 func main() {
-	fmt.Print("화씨 온도 입력 : ")
-	fahrengeit, err := keyboard.GetFloat()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var celsius float64
-	celsius = (fahrengeit - 32) * 5 / 9
-	fmt.Printf("화씨 %.2f도는 섭씨 %.2f도 입니다.", fahrengeit, celsius)
+	greeting.Hello()
+	greeting.Hi()
 }
