1590a9cdd0620f85216eabdd089f7279a2205f9a
KaL <KaL1222kl@gmail.com>
2025-11-03 17:53:00 +0900

    week10 package v0.5

---
+++ week10/cmd/ch04-ex05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/keyboard"
+) // custom package
+
+func main() {
+	fmt.Print("실수 입력 : ")
+	number, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f\n", number)
+}
