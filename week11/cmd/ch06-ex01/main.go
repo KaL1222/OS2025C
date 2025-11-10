5f8d12d1f092d2f06a3f5a8af752d1e001577781
KaL <KaL1222kl@gmail.com>
2025-11-10 17:38:44 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
 	subjects := []string{"Go", "Javascript", "Python", "Linux"}
-	subjectsSlice := subjects[1:3] //slicing
+	subjectsSlice := subjects[:3] //slicing
+	//subjects[0] = "Java"
+	subjectsSlice[0] = "Database"
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
