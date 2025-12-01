16d745650d9b173390788f0b1ec392477f7b8369
KaL <KaL1222kl@gmail.com>
2025-12-01 18:43:46 +0900

    goroutine v0.5

--- week14/cmd/ch13-ex02/main.go
+++ week14/cmd/ch13-ex02/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func abc(channel chan string) {
+	channel <- "내고향 스페셜\n"
+	channel <- "KBS 뉴스광장\n"
+	channel <- "인간극장\n"
+}
+
+func def(channel chan string) {
+	channel <- "건강의 재구성 썰록(재)\n"
+	channel <- "오늘N\n"
+	channel <- "찾아가는 꾸러기 교실\n"
+
+}
+
 func main() {
 	ch := make(chan int) // int 채널 생성
 
